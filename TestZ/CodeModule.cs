6a7b8c78d942ed10edd83319091f75272bc71211
Table <table@DESKTOP-QN5FKUK>
2022-03-14 13:56:50 -0700

    Add project files.

---
+++ TestZ/CodeModule.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+
+namespace TestZ
+{
+    /// <summary>
+    /// A module that bases around dynamically loading C# code in a runtime environment.
+    /// </summary>
+    public class CodeModule
+    {
+        /// <summary>
+        /// Memory stream that will be used to store the assembly when Loaded.
+        /// </summary>
+        private MemoryStream _assemblyStream = new MemoryStream();
+
+        /// <summary>
+        /// Load code into the module
+        /// </summary>
+        /// <param name="code">The code you wish to load.</param>
+        /// <param name="result">The compile result.</param>
+        /// <returns>True if the code is valid, False if the code is not valid.</returns>
+        public bool Load(string code, out EmitResult result)
+        {
+            // flush the stream
+            _assemblyStream.Flush();
+
+            // create the syntax tree
+            SyntaxTree synTree = CSharpSyntaxTree.ParseText(code);
+            // add all references
+            List<PortableExecutableReference> references = new List<PortableExecutableReference>();
+            foreach (var reference in ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator))
+            {
+                references.Add(MetadataReference.CreateFromFile(reference));
+            }
+
+            // create the compiler
+            var compileResult = CSharpCompilation.Create("DynamicAssemblyTest.dll", new SyntaxTree[] { synTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, optimizationLevel: OptimizationLevel.Release));
+            // copy the assembly to memory
+            result = compileResult.Emit(_assemblyStream);
+
+            // return the status
+            return result.Success;
+        }
+
+        /// <summary>
+        /// Execute the module
+        /// </summary>
+        /// <param name="parameters">Parameters that will be passed to Main</param>
+        /// <returns>The result of function execution.</returns>
+        public object Execute(params string[] parameters)
+        {
+            // check the assembly stream to see if theres any data in it...
+            if (_assemblyStream.Length < 1)
+                return null;
+
+            // get the assembly and invoke the main method, while returning the result of it...
+            var customAssembly = Assembly.Load(_assemblyStream.ToArray());
+            Console.WriteLine("EXECTUING IN LAYER 1");
+            return customAssembly.DefinedTypes.First(x => x.Name == "Program").DeclaredMethods.First(x => x.Name == "Main").Invoke(null, new[] { parameters });
+        }
+    }
+}
---
+++ TestZ/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.Emit;
+
+namespace TestZ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string _code = File.ReadAllText("test.txt");
+
+            CodeModule module = new CodeModule();
+
+            if (module.Load(_code, out EmitResult result))
+            {
+                Console.WriteLine("Success layer 1");
+                module.Execute("Argument 1.", "Argument 2.", "Argument 2.");
+            }
+            else
+            {
+                Console.WriteLine("[----Begin-Failure----]");
+                foreach (var reason in result.Diagnostics)
+                    Console.WriteLine(reason);
+                Console.WriteLine("[----END-Failure----]");
+            }
+        }
+    }
+}
